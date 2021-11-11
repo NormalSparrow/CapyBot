bb1f7f94b7bbcb777bb5b67afd788b0edcefe4a8
Kaden Sparrow <sparrow@pop-os.localdomain>
2021-11-11 14:35:16 +0000

    made funny search2

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,18 @@ use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
 use serenity::model::channel::Message;
 use std::collections::HashMap;
+use std::fs::File;
+
+fn search_game(game: String) -> Option<i64> {
+    let file = File::open("games.json").unwrap();
+    let json: Value = serde_json::from_reader(file).unwrap();
+    for app in json["response"]["apps"].as_array().unwrap() {
+        if app["name"].as_str().unwrap() == game {
+            return Some(app["appid"].as_i64().unwrap());
+        }
+    }
+    None
+}
 
 struct Handler;
 
@@ -13,7 +25,9 @@ struct Handler;
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content.starts_with(PREFIX) {
-            match &*msg.content.replace(PREFIX, "") {
+            let content = &*msg.content.replace(PREFIX, "");
+            let mut cmd = content.split(" ").collect::<Vec<&str>>();
+            match cmd[0] {
                 "bara" => {
                     msg.reply(ctx, "https://www.youtube.com/watch?v=APJZeNY6dKo")
                         .await;
@@ -28,30 +42,37 @@ impl EventHandler for Handler {
                 "EyeBurner" => {
                     msg.reply(ctx, "https://ibb.co/3Frj6m4").await;
                 }
-                "nekopara" => {
-                    msg.reply(ctx,format!("there are {} degenerates playing nekopara\n https://tenor.com/view/capibara-gif-22235175",reqwest::get("https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=1406990")        .await.unwrap()
+                "players" => {
+                    cmd.remove(0);
+                    let game = cmd.join(" ");
+                    msg.reply(ctx,format!("there are {} degenerates playing {}\n https://tenor.com/view/capibara-gif-22235175",reqwest::get(format!("https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={}",search_game(game.clone()).unwrap()))        .await.unwrap()
                 .json::<Value>()
-                .await.unwrap()["response"]["player_count"].as_i64().unwrap())).await;
+                .await.unwrap()["response"]["player_count"].as_i64().unwrap(),game)).await;
                 } //pog
                 "price" => {
+                    cmd.remove(0);
+                    let game = cmd.join(" ");
+                    let id = search_game(game).unwrap();
                     msg.reply(
                         ctx,
                         format!(
-                            "nekopara currently costs {} vbucks :pog:",
-                            reqwest::get(
-                                "http://store.steampowered.com/api/appdetails?appids=1406990"
-                            )
+                            "It costs {}",
+                            reqwest::get(format!(
+                                "http://store.steampowered.com/api/appdetails?appids={}",
+                                id
+                            ))
                             .await
                             .unwrap()
                             .json::<Value>()
                             .await
-                            .unwrap()["1406990"]["data"]["price_overview"]["final_formatted"]
+                            .unwrap()[id.to_string()]["data"]["price_overview"]["final_formatted"]
                                 .as_str()
                                 .unwrap()
                         ),
                     )
                     .await;
-                } //pog
+                }
+
                 _ => {}
             }
         }
